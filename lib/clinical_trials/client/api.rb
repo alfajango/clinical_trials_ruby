f17c5fe39d15ba71096b61a4c0b622e3d6093bd5
Richard Peng <richard@richardpeng.com>
2014-08-19 11:31:41 -0400

    update API to allow search parameters and single entry retrieval

--- lib/clinical_trials/client/api.rb
+++ lib/clinical_trials/client/api.rb
@@ -1,7 +1,7 @@
 module ClinicalTrials
   class Client
     module Api
-      
+
       # Returns search results for given term
       # @param term [String] The term to search for
       # @param options [Hash] A customizable set of options. 
@@ -9,10 +9,18 @@ module ClinicalTrials
       # @see http://clinicaltrials.gov/ct2/info/linking
       # @example
       #   searc("lyme disease")
-      
-      def search(term, options={})
-        term.sub(/\s/, "+")
-        get("search?term=%22#{term}%22&displayxml=true", options)
+
+      def search(parameters, options={})
+        url = "ct2/results?displayxml=true"
+        if parameters
+          url += "&#{parameters.map { |key, val| "#{key}=#{val}"}.join("&")}"
+        end
+        get(url, options)
+      end
+
+      def retrieve(nct_id, options={})
+        url = "show/#{nct_id}?displayxml=true"
+        get(url, options)
       end
     end
   end
